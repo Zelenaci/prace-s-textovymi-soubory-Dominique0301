8de6e6a3516bf4f44e929d2d1c40700e30019f82
Dominik Šlehofer <slehoferdominik@gmail.com>
2021-11-15 17:17:31 +0100

    Domaci Ukol

--- main.py
+++ main.py
@@ -8,34 +8,77 @@ from random import randint, choice
 
 ############################################################################
 
-
-def menu():
-    print("""Co chcete provést?
-1) Převod všech písmen na malá písmena.
-2) Nahrazení zanků jiným znakem.    
-3) Generovat náhodný text.
-""")
-    volba = input("Zadejte číslo pro výběr operace: ")
-    return volba
-
-
-if __name__ == "__main__":
-    volba = menu()
-
-
-try:
-    f1=open(input("Zadejte název vstupního souboru:"),"r")
-except FileNotFoundError:
-    print("Soubor nenalezen!")
-
-
-f2=open(input("Zadejte nazev výstupního souboru:"), "w")
-
-
-
-#generovaní textu s volbou počtu slov
 samohlasky = "aeiyou"
 souhlasky = "qwrtpsdfghjklzxcvbnm"
+
+
+
+def mala():
+    try:
+        f1=open(input("Zadejte název vstupního souboru:"),"r")
+    except FileNotFoundError:
+        print("Soubor nenalezen!")
+    
+    try:
+        f2=open(input("Zadejte název výstupního souboru:"),"w")
+    except FileNotFoundError:
+        print("Soubor nenalezen!")
+
+    text = f1.read().lower()
+    f2.write(text)
+
+    f1.close()
+    f2.close()
+
+
+pocet={}
+
+
+def stat():
+
+    try:
+        f1=open(input("Zadejte název vstupního souboru:"),"r")
+    except FileNotFoundError:
+        print("Soubor nenalezen!")
+    
+    while True:
+        pismeno=f1.read(1)
+        if pismeno=="":
+            break
+
+        if pismeno.isalpha():
+            if pismeno not in pocet.keys():
+                pocet[pismeno]=1
+            else:
+                pocet[pismeno]+=1
+    f1.close()
+    nej=max(pocet.values())
+    for key in sorted(pocet.keys()):
+        print("({0})->{1:2} | {2}".format(key, pocet[key], 20*pocet[key]//nej*"#"))
+
+
+def znaky():
+    try:
+        f1=open(input("Zadejte název vstupního souboru:"),"r")
+    except FileNotFoundError:
+        print("Soubor nenalezen!")
+    
+    try:
+        f2=open(input("Zadejte název výstupního souboru:"),"w")
+    except FileNotFoundError:
+         print("Soubor nenalezen!")
+    while True:
+        znak = f1.read(1)
+        if znak=="":
+            break
+        if znak == "A":
+            znak="&"
+        f2.write(znak)
+    f1.close()
+    f2.close()
+
+
+
 def gen_slov(minchars=1, maxchars=10):
     vysledek = ""
     pocet = randint(minchars, maxchars)
@@ -53,43 +96,48 @@ def gen_slov(minchars=1, maxchars=10):
     return vysledek
 
 
-ps = int(input("Zadejte počet slov ve větě:"))
-
-def gen_vet(minwords=ps, maxwords=ps):
+def gen_vet(minwords, maxwords):
     vysledek = ""
     for i in range(randint(minwords, maxwords)):
         vysledek = vysledek + gen_slov() + " "
     return vysledek + "."
 
-veta = gen_vet()
 
-if volba == "3":
+
+def menu():
+
+    print("""Co chcete provést?
+ 1) Převod všech písmen na malá písmena.
+ 2) Nahrazení zanků jiným znakem.
+ 3) Generovat náhodný text.
+ 4) Statistika znaků
+    """)
+    volba = input("Zadejte číslo pro výběr operace: ")
+    return volba
+
+
+if __name__ == "__main__":
+    volba = menu()
+
+
+if volba == "3":    
+    try:
+         f2=open(input("Zadejte název výstupního souboru:"),"w")
+    except FileNotFoundError:
+        print("Soubor nenalezen!")    
+    gen_slov()
+    ps = int(input("Zadejte počet slov ve větě:"))
+    gen_vet(ps,ps)
+    veta = gen_vet(ps,ps)
     f2.write(veta)
-
-
-
-
-#převod písmen na malá písmena 
-def mala():
-    text = f1.read().lower()
-    f2.write(text)
-
-if volba == "1":
+elif volba == "1":
     mala()
+elif volba=="4":
+    stat()
+elif volba == "2":
+    znaky()
+else:
+    print("Nevhodný výběr.")
 
-
-#převod určitého znaku na jiný znak
-while volba == "2":
-    znak = f1.read(1)
-    if znak == "": 
-        break
-    if znak == "A":
-        znak = "&"
-    f2.write(znak)
-
-
-
-f1.close()
-f2.close()
     
-#Dal bych si 3, protože jsem na to dělal cele odpoledne, ale statistika vyskytu se mi nepodařila. Jinak všechno funguje i když ne asi ideálně.
+#Dal bych si 2 nebo 3, protože jsem na to dělal celé dvě odpoledne, funguje to, ale potřeboval jsem od Vás pomoc.
